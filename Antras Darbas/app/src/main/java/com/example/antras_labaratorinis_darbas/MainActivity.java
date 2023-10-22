4b0fa68ca7e47616dd605847d64b34460d6bdbee
JonasPanceris <jonas.panceris@gmail.com>
2023-10-22 18:19:10 +0300

    4

--- Antras Darbas/app/src/main/java/com/example/antras_labaratorinis_darbas/MainActivity.java
+++ Antras Darbas/app/src/main/java/com/example/antras_labaratorinis_darbas/MainActivity.java
@@ -3,13 +3,27 @@ package com.example.antras_labaratorinis_darbas;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
 
 public class MainActivity extends AppCompatActivity {
+
+    EditText edUserInput;
+    Button butCount;
+    TextView tvMain;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        this.edUserInput=findViewById(R.id.edUserInput);
+        this.butCount=findViewById(R.id.butCount);
+        this.tvMain=findViewById(R.id.tvMain);
     }
-    //sveiki
+
+    public void onButCountClick (View view){
+            int result = TextCounter.countSymbols(this.edUserInput.getText().toString());
+            log.i("CountResult", String.valueOf(result));
+            this.tvMain.setText(String.valueOf(result));
+    }
 }
